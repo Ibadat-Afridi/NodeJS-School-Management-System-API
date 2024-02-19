e44d2d3a20cfc27e23102de2e1c04f20b70be669
Ibadat-Afridi <ibadatkhanafridi7@gmail.com>
2024-02-19 17:18:53 +0500

    Creating Mongoose Schema And Model For Students

---
+++ models/Students.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const Students_Schema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  father_name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  roll_no: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  field_of_study: {
+    type: String,
+    required: true,
+  },
+  last_institute_attended: {
+    type: String,
+    required: false,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  phone_no: {
+    type: String,
+    required: true,
+  },
+  blood_group: {
+    type: String,
+    required: false,
+  },
+});
+
+const Students = mongoose.model("Students", Students_Schema);
